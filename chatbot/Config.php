8d8f2e6fca1f3fe3fc7fdb07e91d41163d8a46ec
Bry-robot <35183084+Bry-robot@users.noreply.github.com>
2020-03-03 12:09:29 +0800

    Create Config.php

---
+++ chatbot/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class Config
+{
+
+    public $log = false;
+    public $multiChatbot = true;
+
+    public $connectionInfo = array(
+        "host" => "localhost",
+        "user" => "chat_youdal_cn",
+        "pass" => "6zP3zzZHL5X3fpJB",
+        "dbName" => "chat_youdal_cn"
+    );
+
+    public $parserInfo = array(
+        "aimlDir" => "aiml"
+    );
+
+    public $botInfo =  array(
+            'name' => "botName",
+            'age' => "1",
+            'owner' => "OLCHEM SOFTWARE TECKNOLOGY",
+            'website' => "chatbot.github.io",
+            'version' => "1.0"
+        );
+
+    public $userInfo = array(
+        "name" => "userName",
+        "age" => 20,
+    );
+
+}
+
+
